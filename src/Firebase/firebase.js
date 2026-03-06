5f6ed1d5a05e51465268d0f6831e81856dbae78c
Bhargav Guthula <Bhargavaguthula013@gmail.com>
2026-03-07 03:05:07 +0530

    remove exposed keys

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfVMoL0rstzz4WePHsZdMmFz0vpyVYDlI",
-  authDomain: "educase-assesment.firebaseapp.com",
-  projectId: "educase-assesment",
-  storageBucket: "educase-assesment.firebasestorage.app",
-  messagingSenderId: "773906691377",
-  appId: "1:773906691377:web:fff65c5828c2d68be62c10",
-  measurementId: "G-MC3JKB506G"
+apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 
